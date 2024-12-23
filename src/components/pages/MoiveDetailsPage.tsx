832647c5cef910ff36ee0fd04cab2f0819f5a191
Tom <kiyotomm99@gmail.com>
2024-12-23 17:57:27 +0900

    making movie details

--- src/components/pages/MoiveDetailsPage.tsx
+++ src/components/pages/MoiveDetailsPage.tsx
@@ -4,17 +4,33 @@ import { useParams } from "react-router-dom";
 const MoiveDetailsPage = () => {
   let { id } = useParams();
 
-  const formatHeroDate = (dateString: string) => {
+  const formatHeroDate = (dateString?: string) => {
+    if (!dateString) {
+      return "loading";
+    }
     const date = new Date(dateString);
     return date.getFullYear().toString();
   };
 
   const { data } = useMovieDetails(id);
   return (
-    <div className="flex">
-      <div className="flex gap-3 text-4xl">
-        <div className="font-bold">{data?.original_title}</div>
-        <div>({formatHeroDate(data!.release_date)})</div>
+    <div className="flex gap-7">
+      <div>
+        <img
+          className="w-[20vw]"
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+        />
+      </div>
+      <div className="flex flex-col gap-3 text-4xl">
+        <div className="flex gap-3">
+          <span className="font-bold">{data?.original_title}</span>
+          <span className="opacity-25 font-extralight">
+            ({formatHeroDate(data?.release_date)})
+          </span>
+        </div>
+        <div>
+          <span className="font-extralight">{data?.release_date}</span>
+        </div>
       </div>
     </div>
   );
