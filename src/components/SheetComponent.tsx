db65f6c3b4637f1baa288a711e4472fd979ca689
Tom <kiyotomm99@gmail.com>
2024-12-30 06:27:58 +0900

    a

--- src/components/SheetComponent.tsx
+++ src/components/SheetComponent.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Title from "./Title";
 
 export function SheetComponent() {
   return (
     <div>
       <Sheet key="top">
         <SheetTrigger asChild>
-          <Button variant="outline">open</Button>
+          <Button variant="outline">
+            <Menu />
+          </Button>
         </SheetTrigger>
         <SheetContent side="top" className="h-screen">
+          <span>
+            <Title />
+          </span>
           {/* <SheetFooter>
             <SheetClose asChild>
               <Button type="submit">Save changes</Button>
